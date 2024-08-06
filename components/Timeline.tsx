046a758eafdb14674d3e2cedaba770f6202c4068
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-08-05 22:20:06 -0700

    Fixed incorrect card size

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -9,52 +9,50 @@ const Timeline = ({
   className?: string;
 }) => {
   return (
-    <div>
-      <section id="timeline">
-        <div
-          className={cn(
-            "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-2 gap-32 max-w-7xl mx-auto ",
-            className
-          )}
-        >
-          {
-            timelineItems.map((item, index) => {
-              if (index % 2 === 0) {
-                return <>
-                  <TimelineItem
-                    key={item.id}
-                    title={item.title}
-                    description={item.description}
-                    id={item.id}
-                  />
-                  <TimelineSupport
-                    key={item.supportId}
-                    title={item.supportTitle}
-                    description={item.supportDescription}
-                    id={item.supportId}
-                  />
-                </>
-              } else {
-                return <>
-                  <TimelineSupport
-                    key={item.supportId}
-                    title={item.supportTitle}
-                    description={item.supportDescription}
-                    id={item.supportId}
-                  />
-                  <TimelineItem
-                    key={item.id}
-                    title={item.title}
-                    description={item.description}
-                    id={item.id}
-                  />
-                </>
-              }
-            })
-          }
-        </div>
-      </section>
-    </div>
+    <section id="timeline">
+      <div
+        className={cn(
+          "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-2 gap-32 max-w-7xl mx-auto ",
+          className
+        )}
+      >
+        {
+          timelineItems.map((item, index) => {
+            if (index % 2 === 0) {
+              return <>
+                <TimelineItem
+                  key={item.id}
+                  title={item.title}
+                  description={item.description}
+                  id={item.id}
+                />
+                <TimelineSupport
+                  key={item.supportId}
+                  title={item.supportTitle}
+                  description={item.supportDescription}
+                  id={item.supportId}
+                />
+              </>
+            } else {
+              return <>
+                <TimelineSupport
+                  key={item.supportId}
+                  title={item.supportTitle}
+                  description={item.supportDescription}
+                  id={item.supportId}
+                />
+                <TimelineItem
+                  key={item.id}
+                  title={item.title}
+                  description={item.description}
+                  id={item.id}
+                />
+              </>
+            }
+          })
+        }
+      </div>
+    </section>
   )
 }
 
