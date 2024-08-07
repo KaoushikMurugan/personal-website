9f7baa58bd4968f5471bd4833495184f64432818
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-08-07 13:36:08 -0700

    Added a bunch more icons

--- components/programming-icons-ribbon.tsx
+++ components/programming-icons-ribbon.tsx
@@ -5,23 +5,41 @@ import CPlusPlusIcon from './programming-icons/cplusplus';
 import PythonIcon from './programming-icons/python';
 import JavaScriptIcon from './programming-icons/javascript';
 import TypescriptIcon from './programming-icons/typescript';
-import { AnimatePresence, motion } from 'framer-motion';
 import { cn } from "@/lib/utils";
 import CSharpIcon from './programming-icons/csharp';
 import BashIcon from './programming-icons/bash';
 import PyTorchIcon from './programming-icons/pytorch';
 import TensorFlowIcon from './programming-icons/tensorflow';
+import HTML5Icon from './programming-icons/html5';
+import NumPyIcon from './programming-icons/numpy';
+import CSS3Icon from './programming-icons/css3';
+import PandasIcon from './programming-icons/pandas';
+import NodeJSIcon from './programming-icons/nodejs';
+import ReactIcon from './programming-icons/react';
+import NextJSIcon from './programming-icons/nextjs';
+import GoogleCloudIcon from './programming-icons/googlecloud';
+import JavaIcon from './programming-icons/java';
+import GodotIcon from './programming-icons/godot';
+import UnityIcon from './programming-icons/unity';
+import LinuxIcon from './programming-icons/linux';
+import OpenGLIcon from './programming-icons/opengl';
+import ROSIcon from './programming-icons/ros';
+import DockerIcon from './programming-icons/docker';
+import GitIcon from './programming-icons/git';
+import RaspberryPiIcon from './programming-icons/raspberrypi';
 
 export const ProgrammingIconsRibbon = ({
   direction = "left",
   speed = "fast",
   pauseOnHover = false,
   className,
+  children,
 }: {
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
   pauseOnHover?: boolean;
   className?: string;
+  children?: React.ReactNode;
 }) => {
   const containerRef = React.useRef<HTMLDivElement>(null);
   const scrollerRef = React.useRef<HTMLUListElement>(null);
@@ -85,17 +103,21 @@ export const ProgrammingIconsRibbon = ({
         className={cn(
           " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
           start && "animate-scroll ",
-          pauseOnHover && "hover:[animation-play-state:paused]"
+          pauseOnHover && "hover:[animation-play-state:paused]",
+          // TODO
+          // speed === "fast" && "hover:[animation-duration:40s]",
+          // speed === "normal" && "hover:[animation-duration:80s]",
+          // speed === "slow" && "hover:[animation-duration:160s]",
         )}
       >
-        <ProgrammingIcons />
+        {children}
       </ul>
       <div className="h-10" /> {/* Spacer */}
     </div>
   );
 };
 
-const ProgrammingIcons = (): JSX.Element => {
+const ProgrammingIconsRibbonStack = (): JSX.Element => {
   const iconProps = {
     height: 80,
     width: 80
@@ -107,17 +129,41 @@ const ProgrammingIcons = (): JSX.Element => {
 
   return (
     <>
-      <CLangIcon {...iconProps} />
-      <JavaScriptIcon {...iconProps} />
-      <PythonIcon {...iconProps} />
-      <CPlusPlusIcon {...iconProps} />
-      <TypescriptIcon {...iconProps} />
-      <BashIcon {...iconProps} />
-      <CSharpIcon {...iconProps} />
-      <PyTorchIcon {...wideIconProps} />
-      <TensorFlowIcon {...iconProps} /> 
+      <ProgrammingIconsRibbon direction="left" speed="normal">
+        <CLangIcon {...iconProps} />
+        <JavaScriptIcon {...iconProps} />
+        <PythonIcon {...iconProps} />
+        <CPlusPlusIcon {...iconProps} />
+        <TypescriptIcon {...iconProps} />
+        <HTML5Icon {...iconProps} />
+        <BashIcon {...iconProps} />
+        <CSharpIcon {...iconProps} />
+        <JavaIcon {...iconProps} />
+        <CSS3Icon {...iconProps} />
+      </ProgrammingIconsRibbon>
+      <ProgrammingIconsRibbon direction="right" speed="slow">
+        <PyTorchIcon {...wideIconProps} />
+        <ReactIcon {...iconProps} />
+        <NumPyIcon {...wideIconProps} />
+        <NodeJSIcon {...iconProps} />
+        <TensorFlowIcon {...iconProps} />
+        <NextJSIcon {...wideIconProps} />
+        <PandasIcon {...iconProps} />
+        <OpenGLIcon {...iconProps} />
+      </ProgrammingIconsRibbon>
+      <ProgrammingIconsRibbon direction="left" speed="normal">
+        <GodotIcon {...iconProps} />
+        <GoogleCloudIcon {...iconProps} />
+        <UnityIcon {...wideIconProps} />
+        <LinuxIcon {...iconProps} />
+        <DockerIcon {...iconProps} />
+        <GitIcon {...iconProps} />
+        <RaspberryPiIcon {...iconProps} />
+        <ROSIcon {...wideIconProps} />
+      </ProgrammingIconsRibbon>
     </>
+
   )
 }
 
-export default ProgrammingIconsRibbon
+export default ProgrammingIconsRibbonStack;
