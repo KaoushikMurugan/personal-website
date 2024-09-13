fd5abf4c0e281897cb98e533adc6cde2abfa36eb
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-09-12 18:12:24 -0700

    Removing Floating Navbar
    Don't need it

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,12 @@ import Grid from "@/components/grid";
 import Hero from "@/components/hero";
 import ProgrammingIconsRibbon from "@/components/programming-icons-ribbon";
 import Timeline from "@/components/timeline";
-import { FloatingNav } from "@/components/ui/floating-navbar";
 import { timelineItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav 
-          navItems={[
-            {name: 'Home', link: '/', icon: <FaHome />}
-          ]}
-        />
         <Hero />
         <ProgrammingIconsRibbon />
         <Grid />
