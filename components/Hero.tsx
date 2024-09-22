17bfafda78dde8f9c036f339355900580ddfab27
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-09-21 17:45:56 -0700

    Chore: Un-commented bg-color fix, fixed indentation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
       </div>
 
       <WavyBackground 
-        // backgroundFill="#000319" 
+        backgroundFill="#000319" 
         speed="slow" 
         waveWidth={50} 
         blur={10} 
@@ -39,53 +39,53 @@ const Hero = () => {
         ]}
       >
 
-      <div className="flex justify-center relative my-[8rem] z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center dark:text-blue-100 text-purple-900 max-w-80">
-          </h2>
-          <TextGenerateEffect 
-            className = "text-center text-[40px] md:text-5xl; lg:text-7xl drop-shadow-lg shadow-slate-300"
-            words = "Kaoushik Murugan"
-            duration = {1.0}
-            colorGradient = {[
-              "from-cyan-400 to-teal-300",
-              "from-teal-300 to-cyan-400"]}
-          />
-          <p className = "text-center text-xl">
-            Welcome to my portfolio website!
-          </p>
-
-          <div className="h-10"/>
-          <a href="#timeline">
-            <MagicButton 
-              title="Show My Work"
-              icon={<FaLocationArrow />}
-              position="right"
+        <div className="flex justify-center relative my-[8rem] z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center dark:text-blue-100 text-purple-900 max-w-80">
+            </h2>
+            <TextGenerateEffect 
+              className = "text-center text-[40px] md:text-5xl; lg:text-7xl drop-shadow-lg shadow-slate-300"
+              words = "Kaoushik Murugan"
+              duration = {1.0}
+              colorGradient = {[
+                "from-cyan-400 to-teal-300",
+                "from-teal-300 to-cyan-400"]}
             />
-          </a>
-          <div className="h-10"/>
-          <div className="flex flex-col sm:flex-row relative justify-center items-center gap-10 text-5xl">
-            <span className="text-3xl">Connect with me!</span>
-            <div className="flex sm:flex-row gap-8">
-              <SocialIcon 
-                platform="linkedin" 
-                link="https://www.linkedin.com/in/kaoushikmurugan/" 
-                className="text-neutral-300"
+            <p className = "text-center text-xl">
+              Welcome to my portfolio website!
+            </p>
+
+            <div className="h-10"/>
+            <a href="#timeline">
+              <MagicButton 
+                title="Show My Work"
+                icon={<FaLocationArrow />}
+                position="right"
               />
-              <SocialIcon 
-                platform="github" 
-                link="https://github.com/KaoushikMurugan" 
-                className="text-neutral-300"
-              />
-              <SocialIcon 
-                platform="email"
-                link="mailto:kaoushikmurugan@gmail.com?&subject=Hello%20Kaoushik!"
-                className="text-neutral-300"
-              />
+            </a>
+            <div className="h-10"/>
+            <div className="flex flex-col sm:flex-row relative justify-center items-center gap-10 text-5xl">
+              <span className="text-3xl">Connect with me!</span>
+              <div className="flex sm:flex-row gap-8">
+                <SocialIcon 
+                  platform="linkedin" 
+                  link="https://www.linkedin.com/in/kaoushikmurugan/" 
+                  className="text-neutral-300"
+                />
+                <SocialIcon 
+                  platform="github" 
+                  link="https://github.com/KaoushikMurugan" 
+                  className="text-neutral-300"
+                />
+                <SocialIcon 
+                  platform="email"
+                  link="mailto:kaoushikmurugan@gmail.com?&subject=Hello%20Kaoushik!"
+                  className="text-neutral-300"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </WavyBackground>
     </div>
   )
