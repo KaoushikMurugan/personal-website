d3ddcea9cc05b73e2b5ec48ed5ea11c2dbe0c62e
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-09-25 15:08:16 -0700

    Feat: changed magicbutton bg-color and added hover scaling animation

--- components/buttons/magic-button.tsx
+++ components/buttons/magic-button.tsx
@@ -14,9 +14,9 @@ const MagicButton = ({
   otherClasses?: string;
 }) => {
   return (
-    <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10">
+    <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10 hover:scale-105 transition duration-200">
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#a5f3fc_0%,#0891b2_50%,#a5f3fc_100%)]" />
-      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2">
+      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-cyan-950 text-white px-7 text-sm font-medium backdrop-blur-3xl gap-2">
         {position === "left" && icon}
         {title}
         {position === "right" && icon}
