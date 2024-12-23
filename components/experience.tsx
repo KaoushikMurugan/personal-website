d4f68893f6fa223f5d05ee3d08fe841b4b28f785
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-12-22 19:59:41 -0800

    Feat: Fixed resume resource link

--- components/experience.tsx
+++ components/experience.tsx
@@ -11,13 +11,17 @@ const Experience = () => {
     <section id="experience">
       <SectionTitle title="Experience" />
       <span className="flex justify-center pt-0 pb-[3.75rem] md:pt-5 md:pb-20">
-        <a href="/Kaoushik_Murugan_Resume.pdf" target="_blank" rel="noreferrer">
-          <MagicButton 
-            title="Get my Resume"
-            icon={<IoDocumentText className='text-xl'/>}
-            position="right"
-          />
-        </a>
+      <a
+        href={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/Kaoushik_Murugan_Resume.pdf`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <MagicButton
+          title="Get my Resume"
+          icon={<IoDocumentText className="text-xl" />}
+          position="right"
+        />
+      </a>
       </span>
       <Timeline timelineItems={timelineElements} lineClassName="bg-amber-400 dark:bg-amber-600" />
       <div className="h-10" /> {/* Spacer */}
